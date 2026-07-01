d632dd5224d6f4193d513a9e0bb7d03c58bc8b5f
void2306 <ksakshi8106@gmail.com>
2026-07-01 17:38:24 +0530

    Fix mixed content using vercel proxy rewrites

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://54.164.153.160:8080";
-export const WS_BASE_URL = "http://54.164.153.160:8080/ws-binge";
+export const API_BASE_URL = "/api";
+export const WS_BASE_URL = "ws://54.164.153.160:8080/ws-binge";
